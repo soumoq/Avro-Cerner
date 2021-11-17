710162236c04a2a854f2bb3ce9a5cffbd3f2e58e
Soumojyoti Ganguly <Soumojyoti.Ganguly@cerner.com>
2021-11-17 09:27:59 +0530

    Complete avro with create .avro file

--- src/main/java/com/pojo/Generation.java
+++ src/main/java/com/pojo/Generation.java
@@ -1,7 +1,17 @@
 package com.pojo;
 
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
 import org.apache.commons.lang.RandomStringUtils;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -10,7 +20,7 @@ import java.util.Scanner;
 public class Generation {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         List<CharSequence> address = new ArrayList<>();
         address.add("Address    1");
 
@@ -37,5 +47,38 @@ public class Generation {
         persion.setName(name);
         persion.setPurchases(phrchases);
 
+        serialize(persion);
+        deSerialize();
     }
+
+    private static void serialize(Persion persion) throws IOException {
+        File file = new File("user.avro");
+        DatumWriter<Persion> userDatum = new SpecificDatumWriter<>(Persion.class);
+        DataFileWriter<Persion> dataFileWriter = new DataFileWriter<>(userDatum);
+        dataFileWriter.create(persion.getSchema(), file);
+        dataFileWriter.append(persion);
+        dataFileWriter.close();
+    }
+
+    private static Persion deSerialize() throws IOException {
+        File file = new File("Output.txt");
+        File avroFile = new File("user.avro");
+
+        if (file.exists()) {
+            file.delete();
+        }
+
+        PrintWriter printWriter = new PrintWriter(new FileWriter("Output.txt", true), true);
+        DatumReader<Persion> datumReader = new SpecificDatumReader<>(Persion.class);
+
+        DataFileReader<Persion> dataFileReader = new DataFileReader<Persion>(avroFile,datumReader);
+        Persion user = null;
+        while (dataFileReader.hasNext()){
+            user = dataFileReader.next(user);
+            System.out.println(user);
+        }
+        printWriter.close();
+        return user;
+    }
+
 }
